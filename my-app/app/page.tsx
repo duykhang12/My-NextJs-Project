68e29b639aeba8bb0092ad6f1b56f3c8032b1159
mattrang323 <khangnguyenduy74@gmail.com>
2024-09-05 16:22:18 +0700

    fetch data on the server with API

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,11 @@
-// `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+export default async function Page() {
+  let data = await fetch("https://api.vercel.app/blog");
+  let posts = await data.json();
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title} - {post.author} - {post.category}</li>
+      ))}
+    </ul>
+  );
 }
